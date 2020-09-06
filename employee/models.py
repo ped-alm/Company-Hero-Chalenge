0d9929798087cdb9417be90b011ec52a6555a33a
Pedro Almeida <pedrohenriquealmeidacosta@gmail.com>
2020-09-06 18:34:54 -0300

    adding user to employee

--- employee/models.py
+++ employee/models.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
 class Employee(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     birth_date = models.DateField()
     rg = models.CharField(max_length=31)
