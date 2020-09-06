a829548e3bf62e061526635fd84781fbe9555157
Pedro Almeida <pedrohenriquealmeidacosta@gmail.com>
2020-09-06 19:04:30 -0300

    adding employee route

---
+++ employee/api/viewsets.py
@@ -0,0 +1,8 @@
+from rest_framework.viewsets import ModelViewSet
+from employee.models import Employee
+from .serializers import EmployeeSerializer
+
+
+class EmployeeViewSet(ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
