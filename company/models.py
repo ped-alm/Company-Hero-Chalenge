4b5f97daefd1d067c56dc7de0700a4c3b7765e01
Pedro Almeida <pedrohenriquealmeidacosta@gmail.com>
2020-09-06 18:52:08 -0300

    setting optional fields

--- company/models.py
+++ company/models.py
@@ -4,10 +4,10 @@ from employee.models import Employee
 
 class Company(models.Model):
     name = models.CharField(max_length=255)
-    trading_name = models.CharField(max_length=255)
+    trading_name = models.CharField(max_length=255, null=True, blank=True)
     cnpj = models.CharField(max_length=31)
-    telephone = models.CharField(max_length=31)
-    employees = models.ManyToManyField(Employee)
+    telephone = models.CharField(max_length=31, null=True, blank=True)
+    employees = models.ManyToManyField(Employee, blank=True)
 
     def __str__(self):
         return self.cnpj
--- employee/models.py
+++ employee/models.py
@@ -5,12 +5,12 @@ from django.db import models
 class Employee(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
-    birth_date = models.DateField()
-    rg = models.CharField(max_length=31)
+    birth_date = models.DateField(null=True, blank=True)
+    rg = models.CharField(max_length=31, null=True, blank=True)
     cpf = models.CharField(max_length=31)
-    telephone = models.CharField(max_length=31)
-    email = models.CharField(max_length=255)
-    job = models.CharField(max_length=255)
+    telephone = models.CharField(max_length=31, null=True, blank=True)
+    email = models.CharField(max_length=255, null=True, blank=True)
+    job = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
         return self.cpf
