d0903ca4fc4ef28c07cb16bb7f2281da47cd5873
Pedro Almeida <pedrohenriquealmeidacosta@gmail.com>
2020-09-07 19:25:50 -0300

    still fixing heroku

--- company_hero_challenge/settings.py
+++ company_hero_challenge/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 from decouple import config
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -116,3 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR.joinpath('staticfiles').__str__()
+
+django_heroku.settings(locals())
