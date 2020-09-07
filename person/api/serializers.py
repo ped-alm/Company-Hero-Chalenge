f717babb1f0a079a5d728ae90f4a854b080cfd95
Pedro Almeida <pedrohenriquealmeidacosta@gmail.com>
2020-09-06 23:44:14 -0300

    adding person create

--- person/api/serializers.py
+++ person/api/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 from person.models import Person
@@ -11,3 +12,17 @@ class PersonSerializer(ModelSerializer):
     class Meta:
         model = Person
         fields = ('first_name', 'last_name', 'email', 'rg', 'cpf', 'telephone')
+
+    def create(self, validated_data):
+        cpf = validated_data['cpf']
+
+        if User.objects.filter(username=cpf).exists():
+            raise serializers.ValidationError("person already registered")
+
+        user = User.objects.create_user(username=cpf, email=validated_data['user']['email'],
+                                        first_name=validated_data['user']['first_name'],
+                                        last_name=validated_data['user']['last_name'])
+        user.set_unusable_password()
+
+        validated_data['user'] = user
+        return Person.objects.create(**validated_data)
